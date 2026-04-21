d7a68600747c895f8b82266e25b3c1b68b40e02b
Parth singh <ps.kuntal2005@gmail.com>
2026-04-22 05:21:33 +0530

    Add BYOCApplication class to initialize Spring Boot application

---
+++ byoc-app/src/main/java/com/opensre/byoc/BYOCApplication.java
@@ -0,0 +1,11 @@
+package com.opensre.byoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BYOCApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BYOCApplication.class, args);
+    }
+}
