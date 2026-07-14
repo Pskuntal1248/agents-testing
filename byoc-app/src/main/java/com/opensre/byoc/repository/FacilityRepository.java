c9dad42b37445631b8de07101dc34b12981b9303
Parth singh <ps.kuntal2005@gmail.com>
2026-07-14 23:27:01 +0530

    feat: Remove Facility and User controllers, models, and repositories

--- byoc-app/src/main/java/com/opensre/byoc/repository/FacilityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.opensre.byoc.repository;
-
-import com.opensre.byoc.model.Facility;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FacilityRepository extends JpaRepository<Facility, Long> {
-}
--- byoc-app/src/main/java/com/opensre/byoc/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.opensre.byoc.repository;
-
-import com.opensre.byoc.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
